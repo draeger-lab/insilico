39b2c23793df8843940c925683d6e423c8a8e7a5
anfark <roman-schulte@gmx.de>
2018-02-22 00:49:34 +0100

    Added JUnit to the project

---
+++ tests/com.insilicolab.launcher.test/src/com/insilicolab/launcher/test/DemoTest.java
@@ -0,0 +1,14 @@
+package com.insilicolab.launcher.test;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class DemoTest extends TestCase {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
