66b2f46c3f5fe47f1e2d440b194d4331a960eef6
anfark <roman-schulte@gmx.de>
2018-04-02 00:20:06 +0200

    removed Kotlin from project :(

--- bundles/org.draegerlab.insilico.launcher/src/org/draegerlab/insilico/launcher/ui/LogoPart.java
+++ bundles/org.draegerlab.insilico.launcher/src/org/draegerlab/insilico/launcher/ui/LogoPart.java
@@ -1,15 +1,31 @@
 package org.draegerlab.insilico.launcher.ui;
 
+
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.services.log.Logger;
 
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+@SuppressWarnings("restriction")
 public class LogoPart {
+	
+	@Inject
+	Stage primaryStage;
+	
+	@Inject
+	Logger logger;
+	
 	@PostConstruct
 	void init(BorderPane parent) {
 		Pane right = new Pane();
 		right.setStyle("-fx-background: #FFFFFF;");
 		parent.setCenter(right);
+		
+		// Setup window
+		primaryStage.setResizable(false);
 	}
 }
