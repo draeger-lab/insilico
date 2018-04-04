a4278a638808dbb77b13b0d81248162c4fac1df3
Matthias Koenig <konigmatt@googlemail.com>
2018-04-04 09:37:31 +0200

    clean eclipse ignore & fixed relative starter path

--- linux_x86_64.sh
+++ linux_x86_64.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
-# starter shortcut
-
+#!/bin/bash
+##################################
+# InSilico Start Script
+##################################
 echo "Starting insilico"
-./releng/org.draegerlab.insilico.product/target/products/insilico/linux/gtk/x86_64/insilico/InSilico
+
+# directory of this script 
+ROOTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $ROOTDIR/releng/org.draegerlab.insilico.product/target/products/insilico/linux/gtk/x86_64/insilico/
+./InSilico
+cd $ROOTDIR
 
